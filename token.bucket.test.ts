a81f2380584d2eb63829b52e1c25648854114827
Krzysztof Szymanski <kr.sz.ktc@gmail.com>
2025-06-06 21:41:09 +0200

    token bucket

---
+++ token.bucket.test.ts
@@ -0,0 +1,45 @@
+import { TokenBucket } from "./token.bucket";
+
+
+// for demo purpose don't want to install @types/node
+function assertEq(first: boolean, second: boolean) {
+  if (first === second) {
+    console.log("✅ OK");
+  } else {
+    console.log("⛔ Err");
+  }
+}
+
+const initTime = new Date('2025-06-06T10:00:00Z').getTime();
+const tokenBucket = new TokenBucket(3, 60, initTime); // 1 token / 20s
+
+
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:00Z'), true); // 3 tokens
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:05Z'), true); // 2 tokens
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:10Z'), true); // 1 token
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:15Z'), false); // 0 token 
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:20Z'), true); // 1 token
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:25Z'), false); // 0 token 
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:30Z'), false); // 0 token 
+assertEq(tokenBucket.canRequest('2025-06-06T10:00:35Z'), false); // 0 token 
+// 2025-06-06T10:00:40Z -> 1 token
+// 2025-06-06T10:01:00Z -> 2 token
+assertEq(tokenBucket.canRequest('2025-06-06T10:01:05Z'), true); // 2 token 
+assertEq(tokenBucket.canRequest('2025-06-06T10:01:10Z'), true); // 1 token 
+assertEq(tokenBucket.canRequest('2025-06-06T10:01:15Z'), false); // 0 token 
+// 2025-06-06T10:01:20Z -> 1 token
+// 2025-06-06T10:01:40Z -> 2 token
+// 2025-06-06T10:02:00Z -> 3 token
+// 2025-06-06T10:03:00Z -> still 3 tokens
+assertEq(tokenBucket.canRequest('2025-06-06T10:04:30Z'), true); // 3 tokens 
+assertEq(tokenBucket.canRequest('2025-06-06T10:04:35Z'), true); // 2 tokens 
+assertEq(tokenBucket.canRequest('2025-06-06T10:04:40Z'), true); // 2 tokens
+assertEq(tokenBucket.canRequest('2025-06-06T10:04:45Z'), true); // 1 tokens 
+assertEq(tokenBucket.canRequest('2025-06-06T10:04:50Z'), false); // 0 tokens 
+
+
+
+
+
+
+
---
+++ token.bucket.ts
@@ -0,0 +1,36 @@
+export class TokenBucket {
+  private capacity: number;
+  private tokens: number;
+  private refillInterval: number;
+  private lastRefill: number;
+
+  constructor(capacity: number, refillInterval: number, lastRefill?: number) {
+    this.capacity = capacity;
+    this.tokens = capacity;
+    this.refillInterval = (refillInterval / capacity) * 1000;
+    this.lastRefill = lastRefill ?? Date.now();
+  }
+
+  private refill(timestamp: number) {
+    const timePassed = timestamp - this.lastRefill;
+    const tokensToAdd = Math.floor(timePassed / this.refillInterval);
+
+    if (tokensToAdd > 0 && timestamp > this.lastRefill) {
+      this.tokens = Math.min(this.capacity, this.tokens + tokensToAdd);
+      this.lastRefill += tokensToAdd * this.refillInterval;
+    }
+
+    console.log(`timePassed: ${timePassed}, tokensToAdd: ${tokensToAdd}, tokens: ${this.tokens}`);
+  }
+
+  canRequest(dateTime: string): boolean {
+    const timestamp = new Date(dateTime).getTime();
+    this.refill(timestamp);
+
+    if (this.tokens >= 1) {
+      this.tokens -= 1;
+      return true;
+    }
+    return false;
+  }
+}
